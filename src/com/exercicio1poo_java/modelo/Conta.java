760ac0a7bae099f02ad88b590678ba0bf383df07
tlima1011 <thiagolima1011@gmail.com>
2026-01-31 23:54:22 -0300

    Criacao de atributos, construtor e metodo toString

--- src/com/exercicio1poo_java/modelo/Conta.java
+++ src/com/exercicio1poo_java/modelo/Conta.java
@@ -1,5 +1,23 @@
 package com.exercicio1poo_java.modelo;
 
 public class Conta {
+	
+	public String Numero;
+	public String Titular; 
+	public Double Saldo; 
+	
+	public Conta(String n, String t) {
+		Numero = n; 
+		Titular = t; 
+		Saldo = 0.0; 
+	}
 
+	@Override
+	public String toString() {
+		return String.format("Conta: "
+				+ "\nNumero: %s"
+				+ ", Titular: %s"
+				+ ", Saldo: $ %.2f", Numero, Titular, Saldo );
+	}
+	
 }
